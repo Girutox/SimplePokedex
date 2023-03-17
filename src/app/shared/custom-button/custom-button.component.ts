96a2172821f40ff9af952df230c5f120045dd609
Giancarlo Illescas <giancarlo.hoyos@gmail.com>
2023-03-17 12:22:08 -0500

    add CSS reset

---
+++ src/app/shared/custom-button/custom-button.component.ts
@@ -0,0 +1,14 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-custom-button',
+  templateUrl: './custom-button.component.html',
+  styleUrls: ['./custom-button.component.scss']
+})
+export class CustomButtonComponent {
+  @Input() label: string | undefined;
+
+  constructor() {
+  }
+
+}
