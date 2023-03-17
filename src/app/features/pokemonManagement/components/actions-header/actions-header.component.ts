e3e2042f1f769832d4cdc207ffa981df8ac66bd1
Giancarlo Illescas <giancarlo.hoyos@gmail.com>
2023-03-17 14:01:58 -0500

    add ACTIONS HEADER component

---
+++ src/app/features/pokemonManagement/components/actions-header/actions-header.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-actions-header',
+  templateUrl: './actions-header.component.html',
+  styleUrls: ['./actions-header.component.scss'],
+})
+export class ActionsHeaderComponent {}
--- src/app/features/pokemonManagement/pokemon-management.module.ts
+++ src/app/features/pokemonManagement/pokemon-management.module.ts
@@ -3,9 +3,14 @@ import { CommonModule } from '@angular/common';
 import { FilterGroupComponent } from './components/filter-group/filter-group.component';
 import { PokemonManagementComponent } from './pages/pokemon-management/pokemon-management.component';
 import { SharedModule } from '../../shared/shared.module';
+import { ActionsHeaderComponent } from './components/actions-header/actions-header.component';
 
 @NgModule({
-  declarations: [FilterGroupComponent, PokemonManagementComponent],
+  declarations: [
+    FilterGroupComponent,
+    PokemonManagementComponent,
+    ActionsHeaderComponent,
+  ],
   imports: [CommonModule, SharedModule],
   exports: [PokemonManagementComponent],
 })
