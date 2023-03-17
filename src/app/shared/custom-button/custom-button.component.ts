8065624374d86c809d547bf9941cb26f71d44e9c
Giancarlo Illescas <giancarlo.hoyos@gmail.com>
2023-03-17 12:25:39 -0500

    add CUSTOM BUTTON  component

--- src/app/shared/custom-button/custom-button.component.ts
+++ src/app/shared/custom-button/custom-button.component.ts
@@ -1,14 +1,16 @@
-import {Component, Input} from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ButtonRole } from '../../core/enums/buttonRole.enum';
 
 @Component({
   selector: 'app-custom-button',
   templateUrl: './custom-button.component.html',
-  styleUrls: ['./custom-button.component.scss']
+  styleUrls: ['./custom-button.component.scss'],
 })
 export class CustomButtonComponent {
+  @Input() idRole: number | undefined;
   @Input() label: string | undefined;
 
-  constructor() {
-  }
+  buttonRole = ButtonRole;
 
+  // constructor() {}
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
+
 import { CommonModule } from '@angular/common';
+import { FeatherModule } from 'angular-feather';
+import { Plus, Save, X } from 'angular-feather/icons';
 
+import { CustomButtonComponent } from './custom-button/custom-button.component';
 
+const icons = {
+  Plus,
+  Save,
+  X,
+};
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [CustomButtonComponent],
+  exports: [CustomButtonComponent, FeatherModule],
+  imports: [CommonModule, FeatherModule.pick(icons)],
 })
-export class SharedModule { }
+export class SharedModule {}
